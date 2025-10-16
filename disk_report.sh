1be75fc1e3aa5b5f440742630c9c8c999549415e
Melany Chaparro <mc2266@njit.edu>
2025-10-15 23:16:15 -0400

    Added student header line

--- disk_report.sh
+++ disk_report.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# MY VERSION OF DISK_REPORT.SH BC I HAD COMMIT ISSUES
 echo "Disk report - $(date)" >> ~/disk_usage.log
 df -h >> ~/disk_usage.log
 echo "------------------------" >> ~/disk_usage.log
